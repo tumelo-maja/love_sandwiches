8d43eb4b339d5bc3f19f23ca572bc3976309fa81
tumelo-maja <mogale.ramsey@gmail.com>
2025-04-16 17:26:06 +0100

    Add update_surplus_worksheet function to update the surplus worksheet

--- run.py
+++ run.py
@@ -67,6 +67,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("sales worksheet updated successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the data list provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print("surplus worksheet updated successfully.\n")
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock to calculate the surplus for each item type
@@ -99,7 +108,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation.\n")
 main()
