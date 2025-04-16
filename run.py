5add604064cc99b01232e0e5886bf020f53bb056
tumelo-maja <mogale.ramsey@gmail.com>
2025-04-16 15:33:54 +0100

    Ajust the vaidate_data function to check if all user inputs are integers

--- run.py
+++ run.py
@@ -44,10 +44,11 @@ def validate_data(values):
     print(values)
 
     try:
+        [int(value) for value in values]
         if len(values) !=6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-            )
+            )        
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
