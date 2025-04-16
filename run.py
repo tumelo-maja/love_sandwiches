a55708569c936f870f3a6e646f416f61ab44d34e
tumelo-maja <mogale.ramsey@gmail.com>
2025-04-16 15:20:28 +0100

    Define validate_data function to check if 6 numbers have been entered as input from user

--- run.py
+++ run.py
@@ -28,7 +28,29 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    # print(f"The data provided is {data_str}")
+
+    sale_data = data_str.split(",")
+    print(sale_data)
+    validate_data(sale_data)
+
+def validate_data(values):
+    """
+    Validate data from user input
+    Inside the try, convertes all string values to integers
+    Raises ValueError if strings cannot be converted into int,
+    or if there arent 6 values.
+    """
+    print(values)
+
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 get_sales_data()
